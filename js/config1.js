cef67c2ebe63e95e77bed9291ebc7a4705b268a2
rubendariosantiago <ruben.dario@itesm.mx>
2025-05-03 22:34:23 +0100

    Create config1.js

---
+++ js/config1.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+    tiposEjercicios: ['tipo1', 'tipo2', 'tipo3'],
+    cantidadPreguntasExamen: 5,
+    valoresEnteros: {
+        a: [-3, -2, 2, 3],
+        b: [-3, -2, 2, 3],
+        c: [-3, -2, 2, 3],
+        d: [-2, 2, 3]
+    }
+};
